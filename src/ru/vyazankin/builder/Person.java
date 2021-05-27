132bd4c667338874dfb2a4b4bc214dc7ee8f8294
yacooler <yacooler@yandex.ru>
2021-05-27 22:19:45 +0300

    PersonBuilder done

---
+++ src/ru/vyazankin/builder/Person.java
@@ -0,0 +1,76 @@
+package ru.vyazankin.builder;
+
+public class Person {
+    protected String firstName;
+    protected String lastName;
+    protected String middleName;
+    protected String country;
+    protected String address;
+    protected String phone;
+    protected int age;
+    protected String gender;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
